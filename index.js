5863a39735c852f78b8d8620aee1e364173b61c9
Tony Nguyen <tnguyen411@gmail.com>
2024-09-09 16:12:53 -0700

    Adding sounds

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var length = document.querySelectorAll(".drum").length;
 var audio = new Audio('sounds/tom-1.mp3');
 
 
-
 for (var i = 0; i < length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", audio.play);
 }
